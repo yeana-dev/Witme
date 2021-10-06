7e3a559b14625a7abd9ee2814ae176b436d9901e
yeana0324 <yeana.dev@gmail.com>
2021-10-06 17:48:41 -0500

    Added CSS to navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import Layout from "./components/layout/Layout";
 import MainContainer from "./container/MainContainer";
 import {
--- client/src/components/layout/Navigation.jsx
+++ client/src/components/layout/Navigation.jsx
@@ -1,12 +1,16 @@
+import "../style/Navigation.css";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import { NavLink } from "react-router-dom";
-
+import { default as logosvg } from "./nav_logo.svg";
 function Navigation(props) {
   return (
     // Using bootstrap Navbar component
-    <Navbar bg="light" expand="md">
-      <Navbar.Brand href="/">Witme</Navbar.Brand>
+    <Navbar expand="md" id="navbar">
+      <div id="navbar-logo">
+        <img src={logosvg} width="60" />
+        <div id="navbar-witme">WITME</div>
+      </div>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto navlinks">
