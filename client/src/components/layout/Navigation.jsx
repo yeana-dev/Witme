ef987c609aea059908904f2b44819902a09dc3a4
yeana0324 <yeana.dev@gmail.com>
2021-10-06 22:33:46 -0500

    CSS styling added to home screen

--- client/src/components/layout/Navigation.jsx
+++ client/src/components/layout/Navigation.jsx
@@ -1,32 +1,42 @@
 import "../style/Navigation.css";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import { NavLink } from "react-router-dom";
-import { default as logosvg } from "./nav_logo.svg";
+import { NavLink, Link } from "react-router-dom";
+import { default as logosvg } from "../../assets/nav_logo.svg";
 function Navigation(props) {
   return (
     // Using bootstrap Navbar component
-    <Navbar expand="md" id="navbar">
-      <div id="navbar-logo">
-        <img src={logosvg} width="60" />
-        <div id="navbar-witme">WITME</div>
+    <Navbar expand="lg">
+      <div className="navbar-logo-tabs">
+        <div id="navbar-logo">
+          <img src={logosvg} alt="two-smiley-faces-logo" width="60" />
+          <div id="navbar-witme">WITME</div>
+        </div>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto navlinks">
+            <NavLink to="/" exact={true}>
+              Home
+            </NavLink>
+            <NavLink to="/recruit-side-project">Project teams</NavLink>
+            <NavLink to="/study-group">Study Groups</NavLink>
+          </Nav>
+        </Navbar.Collapse>
       </div>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto navlinks">
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/recruit-side-project">Project teams</NavLink>
-          <NavLink to="/study-group">Study Groups</NavLink>
-          {props.currentUser ? (
-            <div>
-              Hello {props.currentUser.username}!
-              <button onClick={props.handleLogout}>Log Out</button>
-            </div>
-          ) : (
-            <NavLink to="/login">Login</NavLink>
-          )}
-        </Nav>
-      </Navbar.Collapse>
+      {props.currentUser ? (
+        <div className="navbar-user">
+          <span id="navbar-username">
+            👋&nbsp;&nbsp;{props.currentUser.username}
+          </span>
+          <button onClick={props.handleLogout}>Log Out</button>
+        </div>
+      ) : (
+        <div className="navbar-user">
+          <Link to="/login">
+            <button>Login</button>
+          </Link>
+        </div>
+      )}
     </Navbar>
   );
 }
--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -1,5 +1,24 @@
+import "./style/Home.css";
+
 function Home(props) {
-  return <div>I am home</div>;
+  return (
+    <div className="home">
+      <div id="home-title">
+        Bring your team
+        <br />
+        together in Witme
+      </div>
+      <div id="home-title-bottom">
+        A virtual meeting is easier than ever with Witme! <br />
+        Create and meet your team to get{" "}
+        <span className="highlighted">
+          hands-on experience on projects
+        </span>, <br />
+        or join a study group sessions to{" "}
+        <span className="highlighted">level up your motivation!</span>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
