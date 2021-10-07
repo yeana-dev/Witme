5bd9b42a23c408a8ec1e7144190bcdfbf65da30e
yeana0324 <yeana.dev@gmail.com>
2021-10-07 18:25:23 -0500

    Post detail css added for title/username/date

--- client/src/screens/PostDetail.jsx
+++ client/src/screens/PostDetail.jsx
@@ -1,14 +1,27 @@
 import { Link, useParams } from "react-router-dom";
+import "./style/PostDetail.css";
 
 function PostDetail(props) {
   const { id } = useParams();
   const post = props.posts.find((post) => post.id === Number(id));
+  const skillsArray = post.skills.split(",");
+  console.log(skillsArray);
   return (
     <div className="post-detail-container">
-      <div className="post-detail-title">{post.title}</div>
-      <div className="post-detail-user">{post.user.username}</div>
-      <div className="post-detail-date">{post.created_at.slice(0, 10)}</div>
-      <div className="post-detail-skills">{post.skills}</div>
+      <div className="post-detail-top">
+        <div className="post-detail-title">{post.title}</div>
+        <div className="post-detail-username-date">
+          <div className="post-detail-user">{post.user.username}</div>
+          <div className="post-detail-date">{post.created_at.slice(0, 10)}</div>
+        </div>
+      </div>
+      <div className="post-detail-skills">
+        {skillsArray.map((skill, index) => (
+          <div className="skills-button" key={index}>
+            {skill}
+          </div>
+        ))}
+      </div>
       <div className="post-detail-content">{post.content}</div>
       {props.currentUser &&
         post.user.username === props.currentUser.username && (
