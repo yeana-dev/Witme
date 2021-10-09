771abaf96da4b9e95b6f40009eb1035284c6182e
yeana0324 <yeana.dev@gmail.com>
2021-10-08 23:39:26 -0500

    Fixed bug navbar not collapsing after clicking

--- client/src/components/layout/Navigation.jsx
+++ client/src/components/layout/Navigation.jsx
@@ -3,10 +3,12 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import { NavLink, Link } from "react-router-dom";
 import { default as logosvg } from "../../assets/nav_logo.svg";
+import { useState } from "react";
 function Navigation(props) {
+  const [expanded, setExpanded] = useState(false);
   return (
     // Using bootstrap Navbar component
-    <Navbar expand="lg">
+    <Navbar expand="lg" expanded={expanded}>
       <div className="navbar-logo-tabs">
         <div id="navbar-logo">
           <Link to="/">
@@ -18,26 +20,39 @@ function Navigation(props) {
           <Navbar.Toggle
             aria-controls="basic-navbar-nav"
             id="nav-toggle-button"
+            onClick={() => setExpanded(expanded ? false : "expanded")}
           />
         </div>
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto navlinks">
-            <NavLink to="/" exact={true}>
+          <Nav className="me-auto navlinks" id="navbar-menu">
+            <NavLink to="/" exact={true} onClick={() => setExpanded(false)}>
               Home
             </NavLink>
-            <NavLink to="/recruit-side-project">Project teams</NavLink>
-            <NavLink to="/study-group">Study Groups</NavLink>
+            <NavLink
+              to="/recruit-side-project"
+              onClick={() => setExpanded(false)}
+            >
+              Project teams
+            </NavLink>
+            <NavLink to="/study-group" onClick={() => setExpanded(false)}>
+              Study Groups
+            </NavLink>
             {props.currentUser ? (
               <div className="navbar-user">
                 <span id="navbar-username">
                   👋&nbsp;&nbsp;{props.currentUser.username}
                 </span>
-                <button onClick={props.handleLogout}>Log Out</button>
+                <button
+                  onClick={props.handleLogout}
+                  onClick={() => setExpanded(false)}
+                >
+                  Log Out
+                </button>
               </div>
             ) : (
               <div className="navbar-user">
                 <Link to="/login">
-                  <button>Login</button>
+                  <button onClick={() => setExpanded(false)}>Login</button>
                 </Link>
               </div>
             )}
