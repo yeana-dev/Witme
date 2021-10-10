23943fc1eb78ce165513798d3bfefa6b9eb662d4
yeana0324 <yeana.dev@gmail.com>
2021-10-10 15:57:02 -0500

    Added view listing button in Home screen

--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -4,18 +4,32 @@ function Home(props) {
   return (
     <div className="home">
       <div id="home-title">
-        Bring your team
-        <br />
-        together in Witme
+        <header>
+          Bring your team
+          <br />
+          together in Witme
+        </header>
+        <div id="home-title-bottom">
+          A virtual meeting is easier than ever with Witme! <br />
+          Create and meet your team to get{" "}
+          <span className="highlighted">
+            hands-on experience on projects
+          </span>, <br />
+          or join a study group sessions to{" "}
+          <span className="highlighted">level up your motivation!</span>
+        </div>{" "}
       </div>
-      <div id="home-title-bottom">
-        A virtual meeting is easier than ever with Witme! <br />
-        Create and meet your team to get{" "}
-        <span className="highlighted">
-          hands-on experience on projects
-        </span>, <br />
-        or join a study group sessions to{" "}
-        <span className="highlighted">level up your motivation!</span>
+      <div id="home-view-list">
+        <div>
+          Are you a <span className="highlighted">developer</span> or{" "}
+          <span className="highlighted">designer</span> looking to join a team
+          for your next project?<button>View current listing of teams</button>
+        </div>
+        <div>
+          Best way to{" "}
+          <span className="highlighted">eliminates procrastination!</span>
+          <button>View our current study groups</button>
+        </div>
       </div>
     </div>
   );
