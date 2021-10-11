7f23e003c06ea108dd4b192ded403dcb954a19d1
yeana0324 <yeana.dev@gmail.com>
2021-10-10 20:40:54 -0500

    404 page added

---
+++ client/src/screens/PageNotFound.jsx
@@ -0,0 +1,10 @@
+function PageNotFound() {
+  return (
+    <div className="page-not-found">
+      <h1>Oops! Page is not found!</h1>
+      <div>Please head back to homepage!</div>
+    </div>
+  );
+}
+
+export default PageNotFound;
