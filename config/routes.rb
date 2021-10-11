80178d8f114b681bf0685a9c80fe24461c1c2f15
yeana0324 <yeana.dev@gmail.com>
2021-10-10 19:52:47 -0500

    Added route for rendering current users comments

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,12 @@ Rails.application.routes.draw do
 
   resources :comments, except: :create
   get '/posts/:post_id/comments', to: 'comments#showPostsComments'
+  get '/users/:user_id/comments', to: 'comments#showUsersComments'
   post '/posts/:post_id/comments', to: 'comments#create'
 
   resources :posts, except: :create
   post '/posts/:category', to: 'posts#create'
+  get '/users/:user_id/posts', to: 'posts#showUsersPosts'
 
   post 'auth/login', to: 'authentication#login'
   get '/auth/verify', to: 'authentication#verify'
