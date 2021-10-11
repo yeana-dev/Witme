a02cea24ee79bf108ecfed17aa0a70b39d7215d8
yeana0324 <yeana.dev@gmail.com>
2021-10-10 19:54:31 -0500

    Current user can see their posts and comments

--- client/src/screens/User.jsx
+++ client/src/screens/User.jsx
@@ -1,6 +1,82 @@
+import { getUsersPosts } from "../services/posts";
+import { getUsersComments } from "../services/comments";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import PostCard from "../components/PostCard";
+import "./style/User.css";
+
 function User(props) {
-  console.log(props.currentUser);
-  return <h1>I am users page</h1>;
+  const [posts, setPosts] = useState([]);
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const resp = await getUsersPosts(props.currentUser.id);
+      setPosts(resp);
+    };
+    fetchPosts();
+  }, [props.currentUser]);
+
+  useEffect(() => {
+    const fetchComments = async () => {
+      const resp = await getUsersComments(props.currentUser.id);
+      setComments(resp);
+    };
+    fetchComments();
+  }, [props.currentUser]);
+
+  console.log(comments);
+  console.log(posts);
+  return (
+    <div className="user-detail-container">
+      <div className="user-post-container">
+        <header>MY POSTS</header>
+        {posts.map((post) => (
+          <Link
+            to={
+              post.category.name === "side_project"
+                ? `/recruit-side-project/${post.id}`
+                : `/study-group/${post.id}`
+            }
+          >
+            <div className="user-post-card">
+              <div className="title">{post.title}</div>
+              <span className="category">
+                {post.category.name === "side_project"
+                  ? "PROJECT TEAMS"
+                  : "STUDY GROUPS"}
+              </span>{" "}
+              <span className="created_at">{post.created_at.slice(0, 10)}</span>
+            </div>
+          </Link>
+        ))}
+      </div>
+      <header>MY COMMENTS</header>
+      <div className="user-comment-container">
+        {comments.map((comment) => (
+          <Link
+            to={
+              comment.post.category_id === 17
+                ? `/study-group/${comment.post.id}`
+                : `/recruit-side-project/${comment.post.id}`
+            }
+          >
+            <div className="user-comment-card">
+              <div className="content">
+                "{comment.content}"{" "}
+                <span className="created_at">
+                  {comment.created_at.slice(0, 10)}
+                </span>
+                <div className="from-post">
+                  <span className="from">from</span> "{comment.post.title}"
+                </div>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default User;
