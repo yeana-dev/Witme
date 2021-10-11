6b155053744e019bdb8de74aec70f791112eedfc
yeana0324 <yeana.dev@gmail.com>
2021-10-10 21:40:18 -0500

    Added responsive design to post form layout

--- client/src/screens/PostForm.jsx
+++ client/src/screens/PostForm.jsx
@@ -39,75 +39,77 @@ function PostForm(props) {
   console.log(props.currentUser);
 
   return (
-    <form
-      className="create-post"
-      onSubmit={(event) => {
-        event.preventDefault();
-        if (params.id) {
-          props.handlePostUpdate(params.id, post);
-        } else {
-          props.handlePostCreate(post, props.category);
-        }
-      }}
-    >
-      <header>NEW POST</header>
-      <div className="post-form-top">
-        <input
-          type="text"
-          name="title"
-          placeholder="Title"
-          id="form-title"
-          value={post.title}
-          onChange={handleChange}
-        />
-        {props.category === "side_project" && (
-          <select
-            name="looking_for"
-            id="form-looking-for"
+    <div className="post-form-container">
+      <form
+        className="create-post"
+        onSubmit={(event) => {
+          event.preventDefault();
+          if (params.id) {
+            props.handlePostUpdate(params.id, post);
+          } else {
+            props.handlePostCreate(post, props.category);
+          }
+        }}
+      >
+        <header>NEW POST</header>
+        <div className="post-form-top">
+          <input
+            type="text"
+            name="title"
+            placeholder="Title"
+            id="form-title"
+            value={post.title}
             onChange={handleChange}
-            value={post.looking_for}
-          >
-            <option>I am looking for</option>
-            <option value="Front-end">Front-end Developer</option>
-            <option value="Back-end">Back-end Developer</option>
-            <option value="Designer">Designer</option>
-            <option value="All">Above All</option>
-          </select>
-        )}
-        <input
-          type="text"
-          name="skills"
-          id="form-skills"
-          placeholder="Skills/Stacks required. Ex. JavaScript,Figma,Python"
-          value={post.skills}
-          onChange={handleChange}
+          />
+          {props.category === "side_project" && (
+            <select
+              name="looking_for"
+              id="form-looking-for"
+              onChange={handleChange}
+              value={post.looking_for}
+            >
+              <option>I am looking for</option>
+              <option value="Front-end">Front-end Developer</option>
+              <option value="Back-end">Back-end Developer</option>
+              <option value="Designer">Designer</option>
+              <option value="All">Above All</option>
+            </select>
+          )}
+          <input
+            type="text"
+            name="skills"
+            id="form-skills"
+            placeholder="Skills/Stacks required. Ex. JavaScript,Figma,Python"
+            value={post.skills}
+            onChange={handleChange}
+          />
+        </div>
+        <CKEditor
+          name="content"
+          editor={ClassicEditor}
+          data={post.content}
+          config={{
+            removePlugins: [
+              "CKFinderUploadAdapter",
+              "CKFinder",
+              "EasyImage",
+              "Image",
+              "ImageCaption",
+              "ImageStyle",
+              "ImageToolbar",
+            ],
+          }}
+          onChange={(event, editor) => {
+            const data = editor.getData();
+            setPost((prevState) => ({
+              ...prevState,
+              content: data,
+            }));
+          }}
         />
-      </div>
-      <CKEditor
-        name="content"
-        editor={ClassicEditor}
-        data={post.content}
-        config={{
-          removePlugins: [
-            "CKFinderUploadAdapter",
-            "CKFinder",
-            "EasyImage",
-            "Image",
-            "ImageCaption",
-            "ImageStyle",
-            "ImageToolbar",
-          ],
-        }}
-        onChange={(event, editor) => {
-          const data = editor.getData();
-          setPost((prevState) => ({
-            ...prevState,
-            content: data,
-          }));
-        }}
-      />
-      <input type="submit" id="form-submit" />
-    </form>
+        <input type="submit" id="form-submit" />
+      </form>
+    </div>
   );
 }
 
